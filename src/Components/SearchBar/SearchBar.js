774e9b36454f083af0ac4fc87f3333bc91d9cc69
cemyeniceli <cemy@dtu.dk>
2021-04-06 12:38:38 +0200

    added search bar function

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.state = {term: ''}
+    }
+    
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(e) {
+        const newTerm = e.target.value;
+        this.setState({term:newTerm})
+    }
+    
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
-                <button className="SearchButton">SEARCH</button>
+                <input placeholder="Enter A Song, Album, or Artist" 
+                       onChange = {this.handleTermChange}/>
+                <button className="SearchButton" onClick = {this.search}>SEARCH</button>
             </div>
         )
     }
